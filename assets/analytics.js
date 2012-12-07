2097c6d2f3aef421fd110758d1476cfbb812efd0
rdimaggio <robert.dimaggio@gmail.com>
2012-12-07 07:41:40 -0800

    added gosquared analytics tracking code

--- assets/analytics.js
+++ assets/analytics.js
@@ -8,3 +8,19 @@ _gaq.push(['_trackPageview']);
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
+
+var GoSquared = {};
+  GoSquared.acct = "GSN-074093-P";
+  (function(w){
+    function gs(){
+      w._gstc_lt = +new Date;
+      var d = document, g = d.createElement("script");
+      g.type = "text/javascript";
+      g.src = "//d1l6p2sc9645hc.cloudfront.net/tracker.js";
+      var s = d.getElementsByTagName("script")[0];
+      s.parentNode.insertBefore(g, s);
+    }
+    w.addEventListener ?
+      w.addEventListener("load", gs, false) :
+      w.attachEvent("onload", gs);
+  })(window);
